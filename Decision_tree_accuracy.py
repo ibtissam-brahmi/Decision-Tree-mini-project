494f0cd10c77afe916cc366503ad6403bfccabea
BrahmiIbtissam <ibtissambrahmi@gmail.com>
2018-09-08 23:45:02 +0100

    compare_accuracy_in_decision_tree_algo_based_on_different_sizes_of_min_samples_split

---
+++ Decision_tree_accuracy.py
@@ -0,0 +1,79 @@
+import random
+
+
+def makeTerrainData(n_points=1000):
+###############################################################################
+### make the toy dataset
+    random.seed(42)
+    grade = [random.random() for ii in range(0,n_points)]
+    bumpy = [random.random() for ii in range(0,n_points)]
+    error = [random.random() for ii in range(0,n_points)]
+    y = [round(grade[ii]*bumpy[ii]+0.3+0.1*error[ii]) for ii in range(0,n_points)]
+    for ii in range(0, len(y)):
+        if grade[ii]>0.8 or bumpy[ii]>0.8:
+            y[ii] = 1.0
+
+### split into train/test sets
+    X = [[gg, ss] for gg, ss in zip(grade, bumpy)]
+    split = int(0.75*n_points)
+    X_train = X[0:split]
+    X_test  = X[split:]
+    y_train = y[0:split]
+    y_test  = y[split:]
+
+    grade_sig = [X_train[ii][0] for ii in range(0, len(X_train)) if y_train[ii]==0]
+    bumpy_sig = [X_train[ii][1] for ii in range(0, len(X_train)) if y_train[ii]==0]
+    grade_bkg = [X_train[ii][0] for ii in range(0, len(X_train)) if y_train[ii]==1]
+    bumpy_bkg = [X_train[ii][1] for ii in range(0, len(X_train)) if y_train[ii]==1]
+
+    training_data = {"fast":{"grade":grade_sig, "bumpiness":bumpy_sig}
+            , "slow":{"grade":grade_bkg, "bumpiness":bumpy_bkg}}
+
+
+    grade_sig = [X_test[ii][0] for ii in range(0, len(X_test)) if y_test[ii]==0]
+    bumpy_sig = [X_test[ii][1] for ii in range(0, len(X_test)) if y_test[ii]==0]
+    grade_bkg = [X_test[ii][0] for ii in range(0, len(X_test)) if y_test[ii]==1]
+    bumpy_bkg = [X_test[ii][1] for ii in range(0, len(X_test)) if y_test[ii]==1]
+
+    test_data = {"fast":{"grade":grade_sig, "bumpiness":bumpy_sig}
+            , "slow":{"grade":grade_bkg, "bumpiness":bumpy_bkg}}
+
+    return X_train, y_train, X_test, y_test
+
+
+import sys
+#from class_vis import prettyPicture
+#from prep_terrain_data import makeTerrainData
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pylab as pl
+
+features_train, labels_train, features_test, labels_test = makeTerrainData()
+
+
+
+########################## DECISION TREE #################################
+
+
+### your code goes here--now create 2 decision tree classifiers,
+### one with min_samples_split=2 and one with min_samples_split=50
+### compute the accuracies on the testing data and store
+### the accuracy numbers to acc_min_samples_split_2 and
+### acc_min_samples_split_50, respectively
+
+from sklearn import tree
+clf1=tree.DecisionTreeClassifier(min_samples_split=2)
+clf2=tree.DecisionTreeClassifier(min_samples_split=50)
+clf1 = clf1.fit(features_train, labels_train)
+clf2 = clf2.fit(features_train, labels_train)
+pred1 = clf1.predict(features_test)
+pred2 = clf2.predict(features_test)
+from sklearn.metrics import accuracy_score
+acc_min_samples_split_2 = accuracy_score(pred1, labels_test)
+acc_min_samples_split_50 = accuracy_score(pred2, labels_test)
+
+def submitAccuracies():
+  return {"acc_min_samples_split_2":round(acc_min_samples_split_2,3),
+          "acc_min_samples_split_50":round(acc_min_samples_split_50,3)}
+submitAccuracies()
